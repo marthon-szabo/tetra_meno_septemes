e09838c658f3bfccc5d829e9043d17a31fcbbbe4
marthon-szabo <makotoimx@gmail.com>
2020-05-12 13:39:55 +0200

    Add skeleton

---
+++ Hangman2/src/main/java/hu/tetra_meno_septemes/app/MainPage.java
@@ -0,0 +1,25 @@
+package hu.tetra_meno_septemes.app;
+import com.sun.net.httpserver.HttpServer;
+import hu.tetra_meno_septemes.app.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns = {"/"})
+public class MainPage extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest req,
+                      HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("templates/index.html", context, resp.getWriter());
+    }
+
+
+
+}
